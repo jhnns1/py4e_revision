67f97475f77c372970358e866fc396d2bd1f6b08
jhnns1 <joheck1991@gmail.com>
2023-11-08 15:46:40 +0100

    added inheritance

--- oop.py
+++ oop.py
@@ -24,3 +24,15 @@ s.party()
 j = PartyAnimal("John")
 j.party()
 s.party()
+
+# inheritance
+class FootballFan(PartyAnimal):
+   points = 0
+   def touchdown(self):
+      self.points = self.points + 7
+      self.party()
+      print(self.name, " points ", self.points)
+
+f = FootballFan("Frank")
+f.party()
+f.touchdown()
